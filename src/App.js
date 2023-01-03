6dc5da646590f389aef8ad09f97aefbac3d965ab
bisht230 <bishtdeepanshu007@gmail.com>
2023-01-03 23:56:37 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom"
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
 function App() {
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "0.01",
+          },
+        },
+      ],
+    });
+  }
+  const onApprove = (data, actions) =>  {
+    return actions.order.capture();
+  }
   return (
     <div className="App">
-
+      <div className='wrap'>
+       <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+      </div>
      </div>
   );
 }
